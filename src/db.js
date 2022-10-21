0fcdb8c753e38d6141d740d70de01e00f6acff6d
k3v1n <kevinjhosepct@gmail.com>
2022-10-21 02:22:21 -0500

    Config env with heroku

--- src/db.js
+++ src/db.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 const { db } = require("./config");
 
 const pool = new Pool({
-  user: db.user,
-  password: db.password,
-  database: db.database,
-  host: db.host,
-  port: db.port,
+  user: process.env.DB_USER || db.user,
+  password: process.env.DB_PASSWORD || db.password,
+  database: process.env.DB_DATABASE || db.database,
+  host: process.env.DB_HOST || db.host,
+  port: process.env.DB_PORT || db.port,
 });
 
 module.exports = pool;
